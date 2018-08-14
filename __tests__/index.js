731e65be34fb2cfcfaf602d4ef35bd5752851e68
Abdullah Toumeh <abdoullah.tomeh@gmail.com>
2018-08-14 09:08:51 -0700

    first com mit

---
+++ __tests__/index.js
@@ -0,0 +1,55 @@
+// const frogJumps = require('../lib/frog-jumps.js');
+// describe('frogJumps', () => {
+//     describe('when start is equal to end', () => {
+//         it('should return 0', () => {
+//         const result = frogJumps(10, 10, 5);
+//         expect(result).toEqual(0);
+//         });
+//     });
+//     describe('when start is greater to end', () => {
+//         it('should return 0', () => {
+//         const result = frogJumps(100, 10, 5);
+//         expect(result).toEqual(18);
+//         });
+//     });
+//     describe('when start is less than the end', () => {
+//         it('should return the number of jumps, rounded up.', () => {
+//         const result = frogJumps(1, 10, 5);
+//         expect(result).toEqual(2);
+//         });
+//     });
+//     describe('when jump length is 0', () => {
+//         it('should return 0', () => {
+//             const result = frogJumps(1, 10, 0);
+//             expect(result).toEqual(undefined);
+//         });
+//         });
+//     describe('when jump length is less than 0', () => {
+//     it('should return 0', () => {
+//         const result = frogJumps(1, 10, -10);
+//         expect(result).toEqual(undefined);
+//     });
+//     });
+//     describe('when jump length is greater than 0', () => {
+//         it('should return the number of jumps, rounded up', () => {
+//             const result = frogJumps(1, 10, 10);
+//             expect(result).toEqual(1);
+//         });
+//         });
+// });
+
+
+const palindrome = require('../lib/palindrome.js');
+
+  describe('mom', () => {
+    it('mom', () => {
+      expect(palindrome('nev')).toBe(true);
+    });
+  });
+
+
+
+
+
+
+  
